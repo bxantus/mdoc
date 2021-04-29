67f794e188462b8be5e2bbb73aac60fdec5c9459
bxantus <drx.trash@gmail.com>
2021-04-29 16:35:25 +0200

    Added find widget with styles - styles from git graph and vscode combined - match case and regexp options highlight work - search box open close animation works - currently it is always visible, iteraction will be added later

--- www/viewer.js
+++ www/viewer.js
@@ -47,4 +47,72 @@ window.addEventListener('load', () => {
             command: "copyUrl"
         })
     }
+    const findWidget = new FindWidget()
+    findWidget.open()
+    // todo: on `Ctrl+F` should toggle find
+    //       on ESC hide it
 })
+
+class FindWidget {
+    constructor() {
+        this.element = document.createElement("div")
+        this.element.id = "__find"
+        this.element.classList.add("closed")
+        this.input = document.createElement("input")
+        this.input.type = "text"
+        this.input.placeholder = "Find"
+
+        this.findPosition = document.createElement("span")
+        this.findPosition.classList.add("position")
+        this.findPosition.innerHTML = "No Results"
+
+        
+        this.optCaseSensitive = this.createIcon({class:"option case", icon:"case-sensitive", title:"Match Case"})
+        this.optRegex = this.createIcon({class:"option regex", icon:"regex", title:"Use Regular Expression"})
+        this.prevResult = this.createIcon({icon: "arrow-up", title:"Previous match (Shift+Enter)", class:"disabled action"})
+        this.nextResult = this.createIcon({icon: "arrow-down", title:"Next match (Enter)", class:"disabled action"})
+        this.closeIco = this.createIcon({icon: "close", title:"Close (Escape)", class:"action"})
+        
+        const inputContainer = document.createElement("div")
+        inputContainer.classList.add("input-container")
+        inputContainer.append(this.input, this.optCaseSensitive, this.optRegex)
+        this.element.append(inputContainer,
+                            this.findPosition,
+                            this.prevResult, this.nextResult, this.closeIco)
+        this.setup()
+    }
+
+    createIcon(options = { icon:"", class: "", title: "" }) {
+        const ico = document.createElement("i")
+        ico.title = options.title
+        ico.classList.value = options.class
+        ico.classList.add("codicon", `codicon-${options.icon}`)
+        return ico
+    }
+
+    setup() {
+        document.getElementsByTagName("body")[0].append(this.element)
+        this.closeIco.onclick = evt => {
+            this.close()
+        }
+        this.optRegex.onclick = this.optCaseSensitive.onclick = evt => {
+            evt.currentTarget.classList.toggle("active")
+        }
+    }
+
+    open() {
+        const classes = this.element.classList
+        classes.remove("closed")
+        classes.add("open")
+    }
+
+    get isOpen() {
+        this.element.classList.contains("open")
+    }
+
+    close() {
+        const classes = this.element.classList
+        classes.add("closed")
+        classes.remove("open")
+    }
+}
