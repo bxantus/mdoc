0e59c95909d9881c998dc82c52686d8d37bf875b
bxantus <drx.trash@gmail.com>
2021-05-03 14:03:52 +0200

    Find options are taken into account - when regexp errors are encountered the border will change

--- www/viewer.js
+++ www/viewer.js
@@ -58,19 +58,38 @@ window.addEventListener('load', () => {
             findWidget.focus()
         }
     })
-    findWidget.onInputChanged(val => {
-        marker.unmark({done() {
-            if (val != "") {
-                marker.mark(val, {
-                    done: numRes => {
-                        const results = document.querySelectorAll("mark")
-                        findWidget.setResults(results)
-                    }
-                })
-            } else findWidget.setResults([])
-        }})
-    })
+    findWidget.onInputChanged((val, options) => findInPage(findWidget, marker, val, options))
 })
+
+function findInPage(findWidget, marker, text, options) {
+    marker.unmark({
+        done() {
+            if (text == "") {
+                findWidget.setResults([])
+                return
+            }
+            const markOptions = { done: numRes => {
+                                        const results = document.querySelectorAll("mark")
+                                        findWidget.setResults(results)
+                                } 
+                            } 
+            
+            if (options.useRegexp) {
+                try {
+                    const re = new RegExp(text, options.matchCase ? "gm" : "gmi")
+                    marker.markRegExp(re, markOptions)
+                } catch (err) {
+                    findWidget.setResults([])
+                    findWidget.regexpError(err)
+                }
+            } else {
+                markOptions.caseSensitive = options.matchCase
+                marker.mark(text, markOptions)
+            }
+        }
+    })
+    
+}
 
 class FindWidget {
     constructor() {
@@ -99,6 +118,7 @@ class FindWidget {
                             this.findPosition,
                             this.prevResult, this.nextResult, this.closeIco)
         this.results = []
+        this.resultIdx = 1
         this.setup()
     }
 
@@ -118,6 +138,15 @@ class FindWidget {
         this.optRegex.onclick = this.optCaseSensitive.onclick = evt => {
             evt.currentTarget.classList.toggle("active")
             this.focus()
+            this.input.dispatchEvent(new Event("input"))
+        }
+        this.nextResult.onclick = e => this.jumpToResult(this.resultIdx + 1)
+        this.prevResult.onclick = e => this.jumpToResult(this.resultIdx - 1)
+        this.input.onkeydown = keyEvt => {
+            if (keyEvt.key == "Enter") {
+                const delta = keyEvt.shiftKey ? -1 : 1
+                this.jumpToResult(this.resultIdx + delta)
+            }
         }
     }
     
@@ -136,6 +165,12 @@ class FindWidget {
         const classes = this.element.classList
         classes.add("closed")
         classes.remove("open")
+        this.input.value = ""
+        this.input.dispatchEvent(new Event("input"))
+        this.nextResult.classList.add("disabled")
+        this.prevResult.classList.add("disabled")
+        this.results = []
+        this.jumpToResult(0) // will clear result text
     }
 
     focus() {
@@ -143,16 +178,39 @@ class FindWidget {
     }
 
     onInputChanged(listener) {
-        this.input.addEventListener("input",  e => listener(this.input.value))
+        this.input.addEventListener("input",  e => listener(this.input.value, {
+                                                                    matchCase: this.optCaseSensitive.classList.contains("active"),
+                                                                    useRegexp: this.optRegex.classList.contains("active")
+                                                            })
+        )
     }
 
     setResults(results) {
         this.results = results
-        if (results && results.length > 0) {
-            results[0].scrollIntoView()
-            this.findPosition.textContent = `1 of ${results.length}`
+        this.element.classList.remove("error")
+        this.jumpToResult(0)
+        if (results.length > 1) {
+            this.nextResult.classList.remove("disabled")
+            this.prevResult.classList.remove("disabled")
+        } else {
+            this.nextResult.classList.add("disabled")
+            this.prevResult.classList.add("disabled")
+        }
+    }
+
+    jumpToResult(idx) {
+        if (idx < 0) idx = this.results.length - 1
+        else if (idx >= this.results.length) idx = 0
+        this.resultIdx = idx
+        if (this.results.length > 0) {
+            this.results[idx].scrollIntoView()
+            this.findPosition.textContent = `${this.resultIdx + 1} of ${this.results.length}`
         } else {
             this.findPosition.textContent = "No Results"
         }
+    }
+
+    regexpError(err) {
+        this.element.classList.add("error")
     }
 }
